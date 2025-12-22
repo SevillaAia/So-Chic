8136c98c0869a596ee7aadd856ddc3fde09618c9
Guia <guia.sevilla.gs@gmail.com>
2025-12-22 18:29:55 +0100

    footer fix

--- So-Chic/src/components/footer.jsx
+++ So-Chic/src/components/footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import logo from '../assets/SoChicLogo.png'
+import snap from '../assets/snap.png'
+import instagram from '../assets/insta.png'
+import fb from '../assets/fb.png'
 
 function Footer() {
   return (
     <>
     <section className='footer'>
         <div className='address'>
+          <p>Address:</p>
             <address>
                 Storkower Str. 132,<br/>
                 10407, <br/>
@@ -14,9 +18,26 @@ function Footer() {
             <p>Call Us: +49 30 40365894</p>
         </div>
         <div className='socials'>
-            <p>Follow Us</p>
+            <div>
+              <p>Follow Us:</p>
+              <div>
+                <a href="#" target='_blank'>
+                  <img className="socials-logo" src={snap} alt="snap-logo" />
+                </a>
+                <a href="#" target='_blank'>
+                  <img className="socials-logo" src={instagram} alt="insta-logo"></img>
+                </a>
+                <a href="#" target='_blank'>
+                  <img className="socials-logo" src={fb} alt="fb-logo" />
+                </a>
+              </div>
+            </div>
+            
 
-            <a href="https://www.ironhack.com/de-en?utm_source=google&utm_medium=organic&utm_campaign=23-10_LOC_eur_DE_clics_google_mybusiness_&utm_content=organic-content"><img className="footer-logo" src={logo} alt="footer-logo" /></a>
+            <a href="https://www.ironhack.com/de-en?utm_source=google&utm_medium=organic&utm_campaign=23-10_LOC_eur_DE_clics_google_mybusiness_&utm_content=organic-content" target="_blank"><img className="footer-logo" src={logo} alt="footer-logo" /></a>
+
+            <a href="#" target='_blank'></a>
+            
 
         </div>
     </section>
--- So-Chic/src/components/navbar.jsx
+++ So-Chic/src/components/navbar.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import logo from '../assets/SoChicLogo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
-import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import logo from "../assets/SoChicLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
     <>
-    <header className='header'>
-        <button className='account-btn btn'> <FontAwesomeIcon icon={faCircleUser} /></button> 
+      <header className="header">
+        <button className="account-btn btn">
+          <FontAwesomeIcon icon={faCircleUser} />
+        </button>
         <img className="logo" src={logo} alt="SoChic logo" />
-        <div className='head-btns'>
-            <button className='wish-btn btn'><FontAwesomeIcon icon={faHeart} /></button>
-            <button className='cart-btn btn'><FontAwesomeIcon icon={faBasketShopping} /></button>
+        <div className="head-btns">
+          <button className="wish-btn btn">
+            <FontAwesomeIcon icon={faHeart} />
+          </button>
+          <button className="cart-btn btn">
+            <FontAwesomeIcon icon={faBasketShopping} />
+          </button>
         </div>
-    </header>
-    <nav className='navbar'>
-        <div className='nav-li L'>
-            <p>Home</p>
+      </header>
+      <nav className="navbar">
+        <div className="nav-li L">
+          <p>Home</p>
         </div>
-        <div className='nav-li bordered'>
-            <p>About Us</p>
+        <div className="nav-li bordered">
+          <p>About Us</p>
         </div>
-        <div className='nav-li R'>
-            <p>Collections</p>
+        <div className="nav-li R">
+          <p>Collections</p>
         </div>
-    </nav>
+      </nav>
     </>
-)
+  );
 }
 
-export default Navbar
+export default Navbar;
