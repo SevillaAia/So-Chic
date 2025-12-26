b6544269f0328f102bd03ac37a8b39a67bc6e244
Guia <guia.sevilla.gs@gmail.com>
2025-12-26 14:27:33 +0100

    registration

--- So-Chic/src/pages/Register.jsx
+++ So-Chic/src/pages/Register.jsx
@@ -5,7 +5,15 @@ function Register() {
     <div className='reg-container'>
       <div className='reg-form'>
           <h2>Create Your Account</h2>
-          </div>
+          <form className='regForm'>
+            <input className="Email" placeholder="Email" />
+            <input className="Username" placeholder="Username" />
+            <input className="Password" placeholder="Password" />
+            <button className='reg-submit'>
+              Register
+            </button>
+          </form>
+        </div>
     </div>
   )
 }
