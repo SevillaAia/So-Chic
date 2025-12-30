33e230a2275d39d1c43e2049c5852deaa03516ad
Guia <guia.sevilla.gs@gmail.com>
2025-12-30 22:48:48 +0100

    users list

--- So-Chic/src/pages/Users.jsx
+++ So-Chic/src/pages/Users.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Users() {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch users from backend API
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:5005/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h2>Users</h2>
+      <div className="users-list">
+        <ul>
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>
+              <p>Name: {user.username}</p>
+              <p>Email: {user.email}</p>
+              <p>Role: {user.role}</p>
+              <button>Delete User</button>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Users;
